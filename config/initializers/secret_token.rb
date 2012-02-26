8d5735f0f7a49e93f6b813a20aaf89d9ab6470d6
Novikov Andrey <envek@envek.name>
2012-02-26 12:19:53 +1000

    Initial commit: empty application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnergyAudit::Application.config.secret_token = 'e6571c656d7e6185603df3f5ab46e2ab4790d83a7bccbf15bb330ea112b3b8fc5949a4684fadda4f729f2177ac43915a88ed8b616aede24b12fea1cc281c1684'
