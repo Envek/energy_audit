ea3d1fb4a33d5c30549d1031d3f14e8b7077ef35
Novikov Andrey <envek@envek.name>
2012-06-22 00:24:21 +1000

    Auditor activity_values interface. Aggregate by all significant columns.

--- app/controllers/auditor/activity_values_controller.rb
+++ app/controllers/auditor/activity_values_controller.rb
@@ -24,4 +24,16 @@ class Auditor::ActivityValuesController < AuditorController
     render 'subjects'
   end
 
+  def budgets
+    types = [Authority, District, Organisation]
+    @values = {}
+    types.each do |t|
+      value = ActivityValue.joins(:subject).where(:period_id => 4, :subjects => {:type => t.to_s}).group("subjects.type")
+              .select(ActivityValue.significant_column_names.collect { |col| " sum(#{col}) as #{col}" }.join(','))
+              # Well, it was a bit tricky. It just executes sql SUMs for all required columns
+      @values[t.to_s] = value.first if value.any?
+    end
+    render 'budget'
+  end
+
 end
