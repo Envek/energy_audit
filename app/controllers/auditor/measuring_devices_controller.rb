624a4179dddd4bdc7292a21dda6926b2097c25f6
Novikov Andrey <envek@envek.name>
2012-07-03 20:42:56 +1000

    Fix misspelling in queries in auditor measuring devices

--- app/controllers/auditor/measuring_devices_controller.rb
+++ app/controllers/auditor/measuring_devices_controller.rb
@@ -2,21 +2,21 @@ class Auditor::MeasuringDevicesController < AuditorController
   before_filter :load_kinds_and_areas
 
   def districts
-    @areas = Area.includes(:measuring_devices => [:kind, :subject]).where(:measuring_devices => {:period_id => @period.id, :subjects => {:type => 'District'}}).order("areas.id ASC, subjects.id ASC, kinds.id ASC")
+    @areas = Area.includes(:measuring_devices => [:kind, :subject]).where(:measuring_devices => {:period_id => @period.id, :subject_id => {:type => 'District'}}).order("areas.id ASC, subjects.id ASC, kinds.id ASC")
     @subjects = District.all
     @subject_type = District
     render 'report'
   end
   
   def authorities
-    @areas = Area.includes(:measuring_devices => [:kind, :subject]).where(:measuring_devices => {:period_id => @period.id, :subjects => {:type => 'Authority'}}).order("areas.id ASC, subjects.name ASC, kinds.id ASC")
+    @areas = Area.includes(:measuring_devices => [:kind, :subject]).where(:measuring_devices => {:period_id => @period.id, :subject_id => {:type => 'Authority'}}).order("areas.id ASC, subjects.name ASC, kinds.id ASC")
     @use_only_filled_subjects = true
     @subject_type = Authority
     render 'report'
   end
 
   def organisations
-    @areas = Area.includes(:measuring_devices => [:kind, :subject]).where(:measuring_devices => {:period_id => @period.id, :subjects => {:type => 'Organisation'}}).order("areas.id ASC, subjects.name ASC, kinds.id ASC")
+    @areas = Area.includes(:measuring_devices => [:kind, :subject]).where(:measuring_devices => {:period_id => @period.id, :subject_id => {:type => 'Organisation'}}).order("areas.id ASC, subjects.name ASC, kinds.id ASC")
     @use_only_filled_subjects = true
     @subject_type = Organisation
     render 'report'
