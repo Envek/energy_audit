9fa2253015fb47fc2dcf68370e257bc35025b6d5
Novikov Andrey <envek@envek.name>
2012-07-06 14:27:13 +1000

    Fix wrong conditions in ActivityCategory scope

--- app/models/activity_category.rb
+++ app/models/activity_category.rb
@@ -4,7 +4,7 @@ class ActivityCategory < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
 
   scope :with_values, includes(:activities => :activity_values)
-  scope :for_period_and_subject, lambda { |p, s| with_values.where(:activities => { :activity_values => {:period_id => p.id, :subject_id => s.id}})}
+  scope :for_period_and_subject, lambda { |p, s| with_values.where(:activity_values => {:period_id => p.id, :subject_id => s.id})}
   scope :free_for_period_and_subject_with_activity, lambda { |p, s, ac|
     except_activities = Activity.includes(:activity_values).where(:activity_values => {:period_id => p.id, :subject_id => s.id}).select(:id).map{|a| a.id} - [(ac.id if ac)]
     includes(:activities).where('activities.id NOT IN (?)', except_activities.any?? except_activities : 0 )
