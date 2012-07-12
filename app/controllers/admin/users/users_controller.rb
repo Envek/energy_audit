ea665a1fe1fd851a3419495a7dc9249d287ca59f
Novikov Andrey <envek@envek.name>
2012-07-12 15:46:39 +1000

    Use passwords fields in user scaffolds

--- app/controllers/admin/users/users_controller.rb
+++ app/controllers/admin/users/users_controller.rb
@@ -1,8 +1,11 @@
 class Admin::Users::UsersController < ApplicationController
   before_filter :authenticate_admin!
   active_scaffold :user do |conf|
+    conf.columns = [:email, :full_name, :password, :password_confirmation]
     conf.list.columns = [:email, :full_name]
     conf.update.columns = [:email, :full_name, :password, :password_confirmation]
     conf.create.columns = [:email, :full_name, :password, :password_confirmation]
+    conf.columns[:password].form_ui = :password
+    conf.columns[:password_confirmation].form_ui = :password
   end
 end 
