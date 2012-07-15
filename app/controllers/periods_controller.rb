1d8167628c942b98fba916472d6184ed65debc13
Novikov Andrey <envek@envek.name>
2012-07-16 03:07:41 +1000

    Show all periods for admins and operators

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -1,13 +1,22 @@
 class PeriodsController < ApplicationController
+  before_filter :load_periods
 
   def index
-    @periods = Period.published
   end
 
   def show
-    @periods = Period.published
     @period = Period.from_param(params[:id])
     render 'index'
+  end
+
+protected
+
+  def load_periods
+    if current_user and (current_user.admin or current_user.subjects.any?)
+      @periods = Period.all
+    else
+      @periods = Period.published
+    end
   end
 
 end
