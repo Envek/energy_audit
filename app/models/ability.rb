d03103efbc45c3a401c013df2b5896c03f5e2efc
Novikov Andrey <envek@envek.name>
2012-07-23 16:00:52 +1000

    Introduce CanCan gem, write ACL for application

---
+++ app/models/ability.rb
@@ -0,0 +1,23 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user, params)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      cannot :read, User
+      
+      if user.subjects.any?
+        [MeasuringDevice, Audit, ActivityValue, Consumption, Production].each do |stat|
+          can :manage, stat do |stat|
+            user.subject_ids.include? stat.subject_id
+          end
+          cannot :create, stat unless user.subject_ids.include? params[:subject_id].to_i
+        end
+      end
+
+    end
+  end
+end
