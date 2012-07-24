e627f9654266c27ef3c39d9a4bb374e8cefb94ea
Novikov Andrey <envek@envek.name>
2012-07-24 12:00:22 +1000

    And consumptions are cancanized too

--- app/controllers/operator/consumptions_controller.rb
+++ app/controllers/operator/consumptions_controller.rb
@@ -5,10 +5,12 @@ class Operator::ConsumptionsController < OperatorsController
   end
 
   def new
+    authorize! :create, Consumption
     @consumption = Consumption.new(:period => @period, :subject => @subject)
   end
 
   def create
+    authorize! :create, Consumption
     unless Consumption.where(:period_id => @period.id, :subject_id => @subject.id, :resource_id => params[:consumption][:resource_id]).any?
       value = Consumption.new(:period => @period, :subject => @subject)
       value.update_attributes(params[:consumption])
@@ -21,17 +23,20 @@ class Operator::ConsumptionsController < OperatorsController
 
   def edit
     @consumption = Consumption.find(params[:id])
+    authorize! :update, @consumption
   end
 
   def update
     value = Consumption.find(params[:id])
     value.update_attributes(params[:consumption])
+    authorize! :update, value
     value.save!
     redirect_to url_for(:action => :index), :notice => t("operator.consumptions.messages.saved")
   end
 
   def destroy
     value = Consumption.find(params[:id])
+    authorize! :delete, value
     value.destroy
     redirect_to url_for(:action => :index), :notice => t("operator.consumptions.messages.removed")
   end
