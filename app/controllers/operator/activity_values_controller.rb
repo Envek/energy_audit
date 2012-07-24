e010ae5d35c9ebcb7544c6001b8ae95e0ac95624
Novikov Andrey <envek@envek.name>
2012-07-24 11:52:08 +1000

    Activity values are cancanized too

--- app/controllers/operator/activity_values_controller.rb
+++ app/controllers/operator/activity_values_controller.rb
@@ -5,6 +5,7 @@ class Operator::ActivityValuesController < OperatorsController
   end
 
   def new
+    authorize! :create, ActivityValue
     @value = ActivityValue.new
   end
 
@@ -12,6 +13,7 @@ class Operator::ActivityValuesController < OperatorsController
     @value = ActivityValue.new(params[:activity_value])
     @value.period = @period
     @value.subject = @subject
+    authorize! :create, @value
     respond_to do |format|
       if @value.save
         format.html { redirect_to( url_for(:action => :index), :notice => t("operator.activity_values.messages.created") ) }
@@ -23,11 +25,13 @@ class Operator::ActivityValuesController < OperatorsController
 
   def edit
     @value = ActivityValue.find(params[:id])
+    authorize! :update, @value
   end
 
   def update
     @value = ActivityValue.find(params[:id])
     @value.update_attributes(params[:activity_value])
+    authorize! :update, @value
     respond_to do |format|
       if @value.save
         format.html { redirect_to( url_for(:action => :index), :notice => t("operator.activity_values.messages.saved") ) }
@@ -39,10 +43,11 @@ class Operator::ActivityValuesController < OperatorsController
 
   def destroy
     value = ActivityValue.find(params[:id])
+    authorize! :delete, value
     if value.destroy
       redirect_to url_for(:action => :index), :notice => t("operator.activity_values.messages.removed")
     else
-      redirect_to url_for(:action => :index), :error => t("operator.activity_values.messages.cant_remove")
+      redirect_to url_for(:action => :index), :alarm => t("operator.activity_values.messages.cant_remove")
     end
   end
 
