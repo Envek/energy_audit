540c70152a242b0eee1ab43a73a5d77b1b57a3c2
Novikov Andrey <envek@envek.name>
2012-07-24 12:53:34 +1000

    Productions cancan

--- app/controllers/operator/productions_controller.rb
+++ app/controllers/operator/productions_controller.rb
@@ -5,6 +5,7 @@ class Operator::ProductionsController < OperatorsController
   end
 
   def new
+    authorize! :create, Production
     @production = Production.new({:period => @period, :subject => @subject}, :without_protection => true)
   end
 
@@ -12,6 +13,7 @@ class Operator::ProductionsController < OperatorsController
     unless Production.where(:period_id => @period.id, :subject_id => @subject.id, :production_resource_id => params[:production][:production_resource_id]).any?
       @production = Production.new({:period => @period, :subject => @subject}, :without_protection => true)
       @production.update_attributes(params[:production])
+      authorize! :create, @production
       if @production.save
         redirect_to url_for(:action => :index), :notice => t("operator.productions.messages.saved")
       else
@@ -24,10 +26,12 @@ class Operator::ProductionsController < OperatorsController
 
   def edit
     @production = Production.find(params[:id])
+    authorize! :update, @production
   end
 
   def update
     @production = Production.find(params[:id])
+    authorize! :update, @production
     @production.update_attributes(params[:production])
     if @production.save
       redirect_to url_for(:action => :index), :notice => t("operator.productions.messages.saved")
@@ -38,6 +42,7 @@ class Operator::ProductionsController < OperatorsController
 
   def destroy
     value = Production.find(params[:id])
+    authorize! :delete, value
     value.destroy
     redirect_to url_for(:action => :index), :notice => t("operator.productions.messages.removed")
   end
